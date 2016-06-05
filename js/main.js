636ab588c84a58da81481c7bbd48c92d932907b9
Dotan <Dotanavraham88@gmail.com>
2016-06-05 19:38:41 +0300

    fix dropdown boxes
    css almost done!
    
    need validations for name input and check email validation.
    
    add vriable in Json and input to the vriable all the inpust vlidation fields
    and save all in localstorage.
    
    alot of work! GL

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 /**
  * Created by Mor on 01-Jun-16.
  */
+var setNameUrl = document.querySelector('.new-tab');
+
+
+var display = true;
+setNameUrl.onclick = function() {
+    this.parentNode.parentNode.childNodes[3].hidden = display;
+    display = !display;
+
+    this.parentNode.style.backgroundColor = "transparent"
+};
+
+
+var tabs = document.querySelectorAll(".tabs > ul > li");
+
+
+function changeBackground(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.background = "transparent";
+        div[i].style.display = "none";
+    }
+}
+
+var div = document.querySelectorAll(".tabs > div");
+console.log(div);
+
+for (var index in tabs) {
+
+    if (tabs[index].nodeType === 1) {
+            let i = index;
+        console.log(div[i]);
+            tabs[index].onclick = function() {
+                changeBackground(tabs);
+                this.style.background = "rgba(235,235,235,1)";
+                div[i].style.display = "inline-block";
+                console.log(i)
+
+            };
+
+    }
+}
