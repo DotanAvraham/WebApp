cbe17cd01bdd6a0f3122b7bd7ef54c57425cdca4
Dotan <Dotanavraham88@gmail.com>
2016-06-06 10:35:50 +0300

    fix dropdown boxes
    css almost done!
    
    need validations for name input and check email validation.
    
    add vriable in Json and input to the vriable all the inpust vlidation fields
    and save all in localstorage.
    
    alot of work! GL
    
    more little comments on the project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /**
  * Created by Mor on 01-Jun-16.
  */
+
+    ///////////////////   new tab Setting img  ////////////////////////////
 var setNameUrl = document.querySelector('.new-tab');
 
 
@@ -8,14 +10,19 @@ var display = true;
 setNameUrl.onclick = function() {
     this.parentNode.parentNode.childNodes[3].hidden = display;
     display = !display;
-
-    this.parentNode.style.backgroundColor = "transparent"
+    if(!display){
+        this.style.backgroundColor = "transparent";
+    }else{
+        this.style.backgroundColor = "white";
+    }
 };
+
+
 
 
 var tabs = document.querySelectorAll(".tabs > ul > li");
 
-
+///////////////////   set all tabs to transparent background  ////////////////////////////
 function changeBackground(tabs) {
     for (var i = 0; i < tabs.length; i++) {
         tabs[i].style.background = "transparent";
@@ -23,18 +30,21 @@ function changeBackground(tabs) {
     }
 }
 
+
+///////////////////  set and show background for the chosen one    ////////////////////////////
 var div = document.querySelectorAll(".tabs > div");
-console.log(div);
+//console.log(div);
 
 for (var index in tabs) {
 
-    if (tabs[index].nodeType === 1) {
+    if (tabs[index].nodeType === 1) {  // check if <li>
             let i = index;
         console.log(div[i]);
             tabs[index].onclick = function() {
                 changeBackground(tabs);
                 this.style.background = "rgba(235,235,235,1)";
                 div[i].style.display = "inline-block";
+
                 console.log(i)
 
             };
