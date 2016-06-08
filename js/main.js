7fbfdcc922795a0d1afecb9e3e3a866aca2e850b
Dotan <Dotanavraham88@gmail.com>
2016-06-08 17:01:59 +0300

    work on javascript

--- js/main.js
+++ js/main.js
@@ -2,12 +2,36 @@
  * Created by Mor on 01-Jun-16.
  */
 
-    ///////////////////   new tab Setting img  ////////////////////////////
-var setNameUrl = document.querySelector('.new-tab');
+var webApp = {
+    url : {
+        "inputUrl1":" ",
+        "inputUrl2":" ",
+        "inputUrl3":" ",
+        "inputUrl4":" ",
+        "inputUrl5":" ",
+        "inputUrl6":" "
+    },
+    name : {
+        inputName1: " ",
+        inputName2: " ",
+        inputName3: " ",
+        inputName4: " ",
+        inputName5: " ",
+        inputName6: " ",
+    }
+};
 
+    ///////////////////   new tab Setting img  ////////////////////////////
+var setNameUrl = document.querySelectorAll('.setting-tab');
+
+// setNameUrl[0] = the first span
+// setNameUrl[1] = the second span
 
 var display = true;
-setNameUrl.onclick = function() {
+setNameUrl[0].onclick = showOrHideInputs;
+setNameUrl[1].onclick = showOrHideInputs;
+
+    function showOrHideInputs() {
     this.parentNode.parentNode.childNodes[3].hidden = display;
     display = !display;
     if(!display){
@@ -15,10 +39,7 @@ setNameUrl.onclick = function() {
     }else{
         this.style.backgroundColor = "white";
     }
-};
-
-
-
+}
 
 var tabs = document.querySelectorAll(".tabs > ul > li");
 
@@ -30,7 +51,6 @@ function changeBackground(tabs) {
     }
 }
 
-
 ///////////////////  set and show background for the chosen one    ////////////////////////////
 var div = document.querySelectorAll(".tabs > div");
 //console.log(div);
@@ -39,15 +59,75 @@ for (var index in tabs) {
 
     if (tabs[index].nodeType === 1) {  // check if <li>
             let i = index;
-        console.log(div[i]);
+       // console.log(div[i]);
             tabs[index].onclick = function() {
                 changeBackground(tabs);
                 this.style.background = "rgba(235,235,235,1)";
                 div[i].style.display = "inline-block";
 
-                console.log(i)
+              //  console.log(i)
 
             };
 
     }
+}
+
+
+//////////////////////////      check validation inputs and save on object webApp ////////////////////////////
+
+var saveButton = document.querySelectorAll(".submit-button");
+
+
+saveButton[1].onclick = submitButton;
+saveButton[0].onclick = submitButton;
+
+    function submitButton(e){
+
+
+        var input = document.querySelectorAll('.reports-input > input');
+        var arr = Array.prototype.slice.call(input);
+
+
+        /*     */
+
+        this.id === "quick-reports-submit" ?  arr = arr.slice(0,6) : arr = arr.splice(6,arr.length-1);
+/*        if(this.id === "quick-reports-submit"){
+            arr.slice(0,6);
+        }else{
+            arr.splice(6,arr.length-1);
+        }*/
+        checkValidation(arr);
+
+
+   //     arr[0].value = 'blblblbllb';
+
+
+  //  console.log(this.id);
+    //    console.log(arr);
+
+        e.preventDefault();
+
+
+    }
+
+function checkValidation(arr){
+
+    for(var i=0; i < arr.length; i++){
+     //   console.log("index:" +i + " value: " + arr[i].value );
+        // check name fields
+        if(i%2 === 0){
+          //  arr[i].value = "name " + i;
+            if(arr[i].value === "")
+            arr[i].style.required;
+        }
+        // check URL fields
+        else{
+           // arr[i].value = "URL " + i;
+          //  arr[i].style.required = "true";
+        }
+
+
+    }
+
+
 }
